f557ed11435e01d45ce9bcec2df400861b98f7b9
Malcolm Keyes <3.malcolmk@gmail.com>
2022-06-06 16:45:46 -0400

    commit tree b8ea6815a5131f549f128b0a9b1c734f3eee7ab5

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -2,10 +2,29 @@ class Mechanic
 
   attr_reader :name, :specialty
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  @@all = []
 
+  def self.all
+    @@all
+  end
+
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+
+    self.class.all << self
+  end
+
+  def cars
+    Car.all.select {|car| car.mechanic == self }
+  end
+
+  def car_owners
+    cars.map(&:owner).uniq
+  end
+
+  def car_owners_names
+    car_owners.map(&:name)
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,29 @@ def reload
   load 'config/environment.rb'
 end
 
+vette = Car.new("Chevy", "Corvette", "antique")
+beamer = Car.new("BMW", "328i", "clunker")
+trash = Car.new("Nissan", "Altima", "clunker")
+civic = Car.new("Honda", "JDM EF Civic", "exotic")
+
+me = CarOwner.new("mloclam")
+guy = CarOwner.new("fella")
+
+junkie = Mechanic.new("Antonio", "clunker")
+king = Mechanic.new("Richard", "exotic")
+oldhead = Mechanic.new("Paul", "antique")
+
+vette.owner = me
+vette.mechanic = oldhead
+
+beamer.owner = guy
+beamer.mechanic = junkie
+
+trash.owner = guy
+trash.mechanic = junkie
+
+civic.owner = me
+civic.mechanic = king
+
+
 binding.pry
